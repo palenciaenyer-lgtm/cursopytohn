9aa299d03aaae23afe3cdbc9c0520f51ed04aa99
= <=>
2026-03-02 20:48:29 -0400

    actualidad

--- sentenciascontrol.py
+++ sentenciascontrol.py
@@ -1,6 +1,15 @@
 num = int(input("ingrese un numero: "))
 
-if num % 2 == 0:
-    print(f"el numero {num} es par")
+if num != 0:
+     if num > 0:
+        if num % 2 == 0: 
+            print(f"el numero {num} es par positivo")
+        else:
+            print(f"el numero {num} es impar negativo")
+     else:
+         if num % 2 == 0: 
+            print(f"el numero {num} es par negativo")
+         else:
+            print(f"el numero {num} es impar positivo")       
 else:
-    print(f"el numero {num} es impar")
+    print(f"el numero {num} es neutro")
